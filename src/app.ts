a13fa0024559f251f69347205a79ecdef48a4752
Tony Bisegna <tony.bisegna@amadeus.com>
2021-10-24 18:59:19 +0200

    Ignore files already seen (protects again renames)

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import Logger from './modules/logger.js'
 import DiscordBot from './modules/discord_bot.js'
 import chokidar from 'chokidar'
 import config from 'config';
+import hasha from 'hasha';
 
 //const client = new Discord.Client();
 //client.login('token');
+
+Logger.info("Discord Notifier starting...")
 
 if (!config.has("DirectoriesToWatch")) {
     Logger.warning("You must provide the config 'DirectoriesToWatch'")
@@ -31,21 +34,44 @@ for (let discordSetup of config.get("DiscordsBots")) {
     allDiscordsBots.push(aDiscordBot)
 }
 
+let isIgnoringNotif = true;
+setTimeout(() => {
+    isIgnoringNotif = false;
+    Logger.info("Ignore notifs mode disabled!")
+}, config.get('ignoreNotifsAtStartDuringSec')*1000);
 
+
+let alreadySeenHashes = {}
 // Initialize File watcher
 setTimeout(() => {
     const fileWatcher = chokidar.watch(config.get("DirectoriesToWatch"), {
         ignored: /(^|[\/\\])(\.|@)./, // ignore dotfiles and @files
         persistent: true,
-        ignoreInitial: true,
+        ignoreInitial: false,
         usePolling: true,
-        interval: 60,
+        interval: 1000*5,
+        binaryInterval: 1000*5
     });
-    fileWatcher.on('add', (path) => {
+    fileWatcher.on('add', async (path, stats) => {
         let pathSplit = path.split('/');
         let fileName = pathSplit.pop()
         let baseDir = "/"
         let subDir = pathSplit.join('/')
+        let fileHash = await hasha.fromFile(path, {algorithm: 'sha1'})
+        if (alreadySeenHashes[fileHash])
+        {
+            Logger.debug(`Skipping file ${fileName} as hash has already been notified`)
+        }
+        else
+        {
+            alreadySeenHashes[fileHash] = 1;
+            Logger.info(`Flagging file hash of '${fileName}' as already been notified`)
+        }
+        if (isIgnoringNotif)
+        {
+            Logger.debug(`Notif skipped as process is still in ignore mode`)
+            return;
+        }
         for (let aWatchedDir of config.get("DirectoriesToWatch")) {
             if (subDir.indexOf(aWatchedDir) !== -1) {
                 subDir = subDir.replace(aWatchedDir, '')
@@ -62,6 +88,5 @@ setTimeout(() => {
             aDiscordBot.sendNotif(baseDir, subDir, fileName);
         }
     });
-
     Logger.info("Watching directories:", config.get("DirectoriesToWatch"))
 }, 1000)
